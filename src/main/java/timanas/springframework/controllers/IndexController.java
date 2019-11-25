d3587d9f69606cc71963dba4b64b769021890412
Timothy John Manas <timothy.romero.manas@gmail.com>
2019-11-25 16:09:59 -0500

    Created Base Controller and View(HTML)

---
+++ src/main/java/timanas/springframework/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package timanas.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by timmanas on 2019-11-25.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(){
+        return "index";
+    }
+}
