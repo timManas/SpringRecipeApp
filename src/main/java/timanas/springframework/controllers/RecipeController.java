599c0b90537fe0f08f6bba73f4734f0b97cce57a
Timothy John Manas <timothy.romero.manas@gmail.com>
2019-11-27 13:20:06 -0500

    Added form to enter new recipes and insert them to DB

--- src/main/java/timanas/springframework/controllers/RecipeController.java
+++ src/main/java/timanas/springframework/controllers/RecipeController.java
@@ -3,8 +3,11 @@ package timanas.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import timanas.springframework.commands.RecipeCommand;
 import timanas.springframework.services.RecipeService;
 
 @Controller
@@ -20,7 +23,20 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model){
 
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
-
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
