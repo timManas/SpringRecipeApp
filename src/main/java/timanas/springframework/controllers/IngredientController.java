a8da1c248c0be2451ad9397e68690446ebbffb3f
Timothy John Manas <timothy.romero.manas@gmail.com>
2019-11-27 14:34:38 -0500

    Created View Ingredients for seeing list of ingredients

--- src/main/java/timanas/springframework/controllers/IngredientController.java
+++ src/main/java/timanas/springframework/controllers/IngredientController.java
@@ -1,4 +1,28 @@
 package timanas.springframework.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import timanas.springframework.services.RecipeService;
+
+@Controller
 public class IngredientController {
+
+    private final RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping
+    @RequestMapping("/recipe/{recipeId}/ingredients")
+    public String listIngredients(@PathVariable String recipeId, Model model){
+
+        // use command object to avoid lazy load errors in Thymeleaf.
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
+
+        return "recipe/ingredient/list";
+    }
 }
